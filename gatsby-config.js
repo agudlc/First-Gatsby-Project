0edb64177a21cbfe17950b705ccfd403cfb1a76b
agudlc <87042363+agudlc@users.noreply.github.com>
2022-06-05 21:01:53 -0300

    redesign for portfolio project with strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [`gatsby-plugin-image`,
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,{
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/assets/images`,
+      },
+    },`gatsby-plugin-react-helmet`,
+  ],
 }
