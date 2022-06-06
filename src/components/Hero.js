bdd89f24c2c79dc437a2935dbfffa38587b6a37e
agudlc <87042363+agudlc@users.noreply.github.com>
2022-06-06 10:02:34 -0300

    pre-strapi

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,37 @@
-import React from "react"
-import { Link } from "gatsby"
-import socialLinks from "../constants/social_links"
-import heroImg from "../assets/images/hero.svg"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { Link } from "gatsby";
+import socialLinks from "../constants/social_links";
+import { StaticImage } from "gatsby-plugin-image";
+
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm john</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <div className="social-links">
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </article>
+        <StaticImage src="../assets/images/hero.svg" alt="portfolio" className="hero-img" placeholder="blurred"/>
+      </section>
+    </header>
+  )
+
 }
 
 export default Hero
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
-import React from "react"
-import logo from "../assets/images/logo.svg"
-import { FaAlignRight } from "react-icons/fa"
-import pageLinks from "../constants/links"
-import { Link } from "gatsby"
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import { Link } from "gatsby";
+import { FaAlignRight } from "react-icons/fa";
+import pageLinks from "../constants/links";
 const Navbar = () => {
-  return <h2>navbar component</h2>
+    return <nav className = "navbar" >
+                <div className="nav-center" >
+                    <div className="nav-header" >
+                        <img src={logo} alt="logo"/>
+                        <button type="button" className="toggle-btn">
+                             <FaAlignRight></FaAlignRight>
+                        </button>
+                    </div> 
+                    <div className="nav-links">{
+                        pageLinks.map(
+                            (link) => {
+                                return ( 
+                                    <Link key={link.id} to={link.url}> 
+                                    {link.text} 
+                                    </Link>
+                                )
+                            })} 
+                    </div> 
+                </div> 
+            </nav>
 }
 
 export default Navbar
